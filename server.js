3e010aea979f3580cd21afb9c4cbf9f6d54e8e4c
Gary Hsiao <garyh6@users.noreply.github.com>
2020-02-26 23:01:22 -0800

    add cors

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 require("dotenv").config();
+import cors from "cors";
 import express, { json } from "express";
 import mongoose from "mongoose";
+import { corsOptions } from "./cors";
 import auth from "./routes/auth";
 
 const app = express();
 app.use(json());
+app.use(cors(corsOptions));
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
 });
+
 const db = mongoose.connection;
 db.on("error", error => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
